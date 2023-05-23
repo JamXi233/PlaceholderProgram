e17f20d190ba2430917bc7322141f8e5cfc75e57
JamXi <JamSG@qq.com>
2023-05-23 23:07:03 +0800

    initial commit

---
+++ PlaceholderProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceholderProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                Console.Clear();
+                Console.WriteLine("按下回车结束程序\nPress [Enter] Key to end this Program");
+                if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
